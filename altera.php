8d088b1d82f16d17ec033da623895516daa28db9
lean-dro <72211521+lean-dro@users.noreply.github.com>
2021-07-27 09:01:19 -0300

    Correção na aba Status

--- altera.php
+++ altera.php
@@ -3,7 +3,7 @@
     include_once("conexao.php");
     
     $id = $_POST['idAltera'];
-    $update = "update atividade set status='Concluída' where id='$id';";
+    $update = "update atividade set status='Entregue' where id='$id';";
     mysqli_query($conexao, $update) or die (setcookie('resultado', $teste="Sem sucesso ao atualizar.", time()+1));
     setcookie('resultado', $teste="Atividade atualizada.", time()+1);
     mysqli_close($conexao);
